558986a4145b82423d3ff86ae94036ea32073e60
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-08-18 05:06:28 +0800

    added js functions for show and hide of menu in small screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,30 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
 
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-
+/*===== MENU HIDE =====*/
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
 
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(item => item.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
 
