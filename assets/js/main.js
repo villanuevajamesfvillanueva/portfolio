379bd630fa5f59c577a26dc9fa556123f03afb6a
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-08-18 13:34:22 +0800

    added skills section

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,8 +27,24 @@ function linkAction() {
 navLink.forEach(item => item.addEventListener('click', linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
 
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
 
+    for (let i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills);
+});
 /*==================== QUALIFICATION TABS ====================*/
 
 
