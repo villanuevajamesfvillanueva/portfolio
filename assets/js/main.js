6cd5e103827b4610f25bec8f6fdd87f21defd7c7
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-08-19 23:35:47 +0800

    added media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,31 +110,31 @@ window.addEventListener('scroll', scrollUp);
 
 
 // -------------------- dark light theme -------------------- 
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'uil-sun';
+// const themeButton = document.getElementById('theme-button');
+// const darkTheme = 'dark-theme';
+// const iconTheme = 'uil-sun';
 
 // Previously selected theme (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
+// const selectedTheme = localStorage.getItem('selected-theme');
+// const selectedIcon = localStorage.getItem('selected-icon');
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+// const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+// const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
 
 // We validate if the user previously chose a theme
-if (selectedTheme) {
+// if (selectedTheme) {
     // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-    (selectedTheme === 'dark') ? document.body.classList.add(darkTheme) : document.body.classList.remove(darkTheme);
-    (selectedIcon === 'uil-moon') ? themeButton.classList.add(iconTheme) : themeButton.classList.remove(iconTheme);
-}
+//     (selectedTheme === 'dark') ? document.body.classList.add(darkTheme) : document.body.classList.remove(darkTheme);
+//     (selectedIcon === 'uil-moon') ? themeButton.classList.add(iconTheme) : themeButton.classList.remove(iconTheme);
+// }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
+// themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme);
-    themeButton.classList.toggle(iconTheme);
+//     document.body.classList.toggle(darkTheme);
+//     themeButton.classList.toggle(iconTheme);
     // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme());
-    localStorage.setItem('selected-icon', getCurrentIcon());
-});
+//     localStorage.setItem('selected-theme', getCurrentTheme());
+//     localStorage.setItem('selected-icon', getCurrentIcon());
+//});
